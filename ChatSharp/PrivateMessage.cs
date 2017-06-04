0a502f5d3cfe26283b9cf5ba39bd010b172d64dd
Andrej <anhr@mail.ru>
2017-06-04 11:27:57 -0700

    Revert "PrivateMessage.Receiver"
    This reverts commit bd90c7fb5332382fc96c878d7bcfffc028bb6fc4.

--- ChatSharp/PrivateMessage.cs
+++ ChatSharp/PrivateMessage.cs
@@ -18,10 +18,7 @@ namespace ChatSharp
             if (serverInfo.ChannelTypes.Any(c => Source.StartsWith(c.ToString())))
                 IsChannelMessage = true;
             else
-            {
                 Source = User.Nick;
-                Receiver = message.Parameters[0];
-            }
         }
 
         /// <summary>
@@ -36,10 +33,6 @@ namespace ChatSharp
         /// The source of the message (a nick or a channel name).
         /// </summary>
         public string Source { get; set; }
-        /// <summary>
-        /// The nickname of the receiver of the message.
-        /// </summary>
-        public string Receiver { get; set; }
         /// <summary>
         /// True if this message was posted to a channel.
         /// </summary>
