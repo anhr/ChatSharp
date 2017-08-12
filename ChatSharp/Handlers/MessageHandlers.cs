f3c0a1cab45cc93dda29afcaf25928968b6478ce
Andrej <anhr@mail.ru>
2017-08-11 21:50:23 -0700

    493 ERR_NOFEATURE handler

--- ChatSharp/Handlers/MessageHandlers.cs
+++ ChatSharp/Handlers/MessageHandlers.cs
@@ -34,8 +34,8 @@ namespace ChatSharp.Handlers
             // Channel handlers
             client.SetHandler("JOIN", ChannelHandlers.HandleJoin);
             client.SetHandler("PART", ChannelHandlers.HandlePart);
-            client.SetHandler("332", ChannelHandlers.HandleGetTopic);
             client.SetHandler("331", ChannelHandlers.HandleGetEmptyTopic);
+            client.SetHandler("332", ChannelHandlers.HandleGetTopic);
             client.SetHandler("353", ChannelHandlers.HandleUserListPart);
             client.SetHandler("366", ChannelHandlers.HandleUserListEnd);
             client.SetHandler("KICK", ChannelHandlers.HandleKick);
@@ -90,7 +90,8 @@ namespace ChatSharp.Handlers
             client.SetHandler("479", ErrorHandlers.HandleError);//"<channel> :Illegal channel name
             client.SetHandler("482", ErrorHandlers.HandleError);//ERR_CHANOPRIVSNEEDED "<channel> :You're not channel operator" - Any command requiring 'chanop' privileges(such as MODE messages) must return this error if the client making the attempt is not a chanop on the specified channel.
             client.SetHandler("485", ErrorHandlers.HandleError);//ERR_UNIQOPRIVSNEEDED	RFC2812	:<reason>	Any mode requiring 'channel creator' privileges returns this error if the client is attempting to use it while not a channel creator on the given channel
-            client.SetHandler("494", ErrorHandlers.HandleError);//ERR_BADFEATURE
+            client.SetHandler("493", ErrorHandlers.HandleError);//ERR_NOFEATURE The user does not wish to receive that type of message.
+            client.SetHandler("494", ErrorHandlers.HandleError);//ERR_BADFEATURE Your own modes prohibit you from sending that type of message.
             client.SetHandler("513", ErrorHandlers.HandleError);//ERR_BADPING Also known as ERR_NEEDPONG (Unreal/Ultimate) for use during registration, however it's not used in Unreal (and might not be used in Ultimate either).
             client.SetHandler("538", ErrorHandlers.HandleError);//"<channel1> is linked to <channel2> but <channel2> is not accepting links from <channel1>." reply from irc.swiftirc.net IRC server
 
